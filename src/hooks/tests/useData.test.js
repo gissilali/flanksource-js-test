68fc6b6513724272db333b14db9fc30918a1e031
Gibson Silali <gibsonsilali@gmail.com>
2022-11-04 10:49:45 +0300

    initial commit

---
+++ src/hooks/tests/useData.test.js
@@ -0,0 +1,6 @@
+import { useData } from "../../hooks/useData";
+import { renderHook, act } from "@testing-library/react";
+
+test("should extract a standings table from fixtures array", () => {
+  expect(true).toBe(true);
+});
